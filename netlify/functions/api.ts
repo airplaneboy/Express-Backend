88335e628bd3f934fc9d38c32ad9912435238506
Sulaiman Agara <airplaneboy00@gmail.com>
2024-06-09 12:15:08 -0400

    added mongo connection

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -3,6 +3,7 @@
 import express, { Router } from 'express';
 import serverless from 'serverless-http';
 import { app } from '../../app';
+const connectDB = require('../../database/connectDB');
 const api = express();
 
 const router = Router();
@@ -10,4 +11,5 @@ router.get('/hello', (req, res) => res.send('Hello World!'));
 
 app.use('/api/', router);
 
+await connectDB(process.env.MONGO_URI);
 export const handler = serverless(app);

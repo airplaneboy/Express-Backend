a7d80665eccd8d555b82339968c60b63ac2aac92
Sulaiman Agara <airplaneboy00@gmail.com>
2024-06-09 12:48:06 -0400

    fix #4

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/hello', (req, res) => res.send('Hello World!'));
 router.get('/', (req, res) => res.send('Welcome!'));
 
-app.use('/api/', router);
+app.use('/', router);
 
 const start = async () => {
   try {
